a2f59d5430f5d60808598a1dc00edd953c5ec103
Hang Lu <lvhangrobin@gmail.com>
2013-03-31 15:45:19 -0400

    change it to be valid on server

--- cis551hw3/src/Client/ChatClient.java
+++ cis551hw3/src/Client/ChatClient.java
@@ -7,7 +7,11 @@ public class ChatClient {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String hostName = "localhost";
+		if(args.length!=1){
+			System.out.println("Invalid arguments");
+			System.exit(0);
+		}
+		String hostName = args[0];
 		int portNumber = 30130;
 		ChatClientImpl client = new ChatClientImpl();
 		client.Start(hostName, portNumber);
